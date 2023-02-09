31efaf90f85c14956ebb26afecd700d2b2c3f47d
LdSaGL <lucas.sa.licio@gmail.com>
2023-02-09 17:22:40 -0300

    Arquivos sql

---
+++ banco.sql
@@ -0,0 +1,195 @@
+CREATE TABLE Musica (
+    id_music CHAR PRIMARY KEY,
+    nm_musica VARCHAR,
+    clipe BYTEA,
+    letra VARCHAR,
+    idioma VARCHAR,
+    fk_Artista_cd_artista CHAR,
+    fk_Genero_cd_genero CHAR,
+    fk_Album_cd_album CHAR
+);
+
+CREATE TABLE Album (
+    id_album CHAR PRIMARY KEY,
+    nm_album VARCHAR,
+    dt_criacao DATE,
+    capa BYTEA,
+    fk_Produtora_cd_produtora CHAR,
+    fk_Artista_cd_artista CHAR
+);
+
+CREATE TABLE Artista (
+    id_artista CHAR PRIMARY KEY,
+    nm_artista VARCHAR,
+    dt_nascimento DATE,
+    foto BYTEA
+);
+
+CREATE TABLE Playlist (
+    id_playlist CHAR PRIMARY KEY,
+    nm_playlist VARCHAR,
+    capa BYTEA,
+    dt_criacao DATE,
+    fk_Usuario_cd_user CHAR
+);
+
+CREATE TABLE Usuario (
+    id_user CHAR PRIMARY KEY,
+    nm_user VARCHAR,
+    dt_nascimento DATE,
+    email VARCHAR,
+    senha VARCHAR,
+    telefone VARCHAR,
+    foto BYTEA,
+    is_admin BOOLEAN
+);
+
+CREATE TABLE Genero (
+    id_genero CHAR PRIMARY KEY,
+    nm_genero VARCHAR,
+    descricao VARCHAR
+);
+
+CREATE TABLE Grupos (
+    id_grupo CHAR PRIMARY KEY,
+    nm_grupo VARCHAR,
+    dt_criacao DATE
+);
+
+CREATE TABLE Produtora (
+    id_produtora CHAR PRIMARY KEY,
+    nm_produtora VARCHAR,
+    foto BYTEA
+);
+
+CREATE TABLE Radio (
+    id_radio CHAR PRIMARY KEY,
+    nm_radio VARCHAR
+);
+
+CREATE TABLE Show (
+    id_show CHAR PRIMARY KEY,
+    video BYTEA,
+    nm_show VARCHAR,
+    dt_criacao DATE
+);
+
+CREATE TABLE User_musica (
+    fk_Usuario_cd_user CHAR,
+    fk_Musica_cd_musica CHAR
+);
+
+CREATE TABLE Radio_Musica (
+    fk_Radio_id_radio CHAR,
+    fk_Musica_cd_musica CHAR
+);
+
+CREATE TABLE Show_Musica (
+    fk_Musica_cd_musica CHAR,
+    fk_Show_id_show CHAR
+);
+
+CREATE TABLE Playlist_Musica (
+    fk_Musica_cd_musica CHAR,
+    fk_Playlist_cd_playlist CHAR
+);
+
+CREATE TABLE Grupos_Usuario (
+    fk_Usuario_cd_user CHAR,
+    fk_Grupos_cd_grupo CHAR
+);
+
+CREATE TABLE Show_Artista (
+    fk_Show_id_show CHAR,
+    fk_Artista_cd_artista CHAR
+);
+ 
+ALTER TABLE Musica ADD CONSTRAINT FK_Musica_2
+    FOREIGN KEY (fk_Artista_cd_artista)
+    REFERENCES Artista (cd_artista)
+    ON DELETE RESTRICT;
+ 
+ALTER TABLE Musica ADD CONSTRAINT FK_Musica_3
+    FOREIGN KEY (fk_Genero_cd_genero)
+    REFERENCES Genero (cd_genero)
+    ON DELETE RESTRICT;
+ 
+ALTER TABLE Musica ADD CONSTRAINT FK_Musica_4
+    FOREIGN KEY (fk_Album_cd_album)
+    REFERENCES Album (cd_album)
+    ON DELETE RESTRICT;
+ 
+ALTER TABLE Album ADD CONSTRAINT FK_Album_2
+    FOREIGN KEY (fk_Produtora_cd_produtora)
+    REFERENCES Produtora (cd_produtora)
+    ON DELETE RESTRICT;
+
+ALTER TABLE Album ADD CONSTRAINT FK_Album_3
+    FOREIGN KEY (fk_Artista_cd_artista)
+    REFERENCES Artista (cd_artista)
+    ON DELETE RESTRICT;
+ 
+ALTER TABLE Playlist ADD CONSTRAINT FK_Playlist_2
+    FOREIGN KEY (fk_Usuario_cd_user)
+    REFERENCES Usuario (cd_user)
+    ON DELETE RESTRICT;
+ 
+ALTER TABLE User_musica ADD CONSTRAINT FK_User_musica_1
+    FOREIGN KEY (fk_Usuario_cd_user)
+    REFERENCES Usuario (cd_user)
+    ON DELETE RESTRICT;
+ 
+ALTER TABLE User_musica ADD CONSTRAINT FK_User_musica_2
+    FOREIGN KEY (fk_Musica_cd_musica)
+    REFERENCES Musica (cd_musica)
+    ON DELETE RESTRICT;
+ 
+ALTER TABLE Radio_Musica ADD CONSTRAINT FK_Radio_Musica_1
+    FOREIGN KEY (fk_Radio_id_radio)
+    REFERENCES Radio (id_radio)
+    ON DELETE RESTRICT;
+ 
+ALTER TABLE Radio_Musica ADD CONSTRAINT FK_Radio_Musica_2
+    FOREIGN KEY (fk_Musica_cd_musica)
+    REFERENCES Musica (cd_musica)
+    ON DELETE RESTRICT;
+ 
+ALTER TABLE Show_Musica ADD CONSTRAINT FK_Show_Musica_1
+    FOREIGN KEY (fk_Musica_cd_musica)
+    REFERENCES Musica (cd_musica)
+    ON DELETE RESTRICT;
+ 
+ALTER TABLE Show_Musica ADD CONSTRAINT FK_Show_Musica_2
+    FOREIGN KEY (fk_Show_id_show)
+    REFERENCES Show (id_show)
+    ON DELETE RESTRICT;
+ 
+ALTER TABLE Playlist_Musica ADD CONSTRAINT FK_Playlist_Musica_1
+    FOREIGN KEY (fk_Musica_cd_musica)
+    REFERENCES Musica (cd_musica)
+    ON DELETE RESTRICT;
+ 
+ALTER TABLE Playlist_Musica ADD CONSTRAINT FK_Playlist_Musica_2
+    FOREIGN KEY (fk_Playlist_cd_playlist)
+    REFERENCES Playlist (cd_playlist)
+    ON DELETE RESTRICT;
+ 
+ALTER TABLE Grupos_Usuario ADD CONSTRAINT FK_Grupos_Usuario_1
+    FOREIGN KEY (fk_Usuario_cd_user)
+    REFERENCES Usuario (cd_user)
+    ON DELETE RESTRICT;
+ 
+ALTER TABLE Grupos_Usuario ADD CONSTRAINT FK_Grupos_Usuario_2
+    FOREIGN KEY (fk_Grupos_cd_grupo)
+    REFERENCES Grupos (cd_grupo)
+    ON DELETE RESTRICT;
+ 
+ALTER TABLE Show_Artista ADD CONSTRAINT FK_Show_Artista_1
+    FOREIGN KEY (fk_Show_id_show)
+    REFERENCES Show (id_show)
+    ON DELETE RESTRICT;
+ 
+ALTER TABLE Show_Artista ADD CONSTRAINT FK_Show_Artista_2
+    FOREIGN KEY (fk_Artista_cd_artista)
+    REFERENCES Artista (cd_artista)
+    ON DELETE RESTRICT;
---
+++ insercoes.sql
@@ -0,0 +1,74 @@
+INSERT INTO producer (id_producer, producer_name, photo)
+VALUES (1, 'sony', NULL),
+       (2, '30praum', NULL),
+       (3, 'universal', NULL),
+	   (4, 'globo', NULL),
+	   (5, 'disney', NULL);
+
+INSERT INTO artist (id_artist, artist_name, date_of_birth, photo)
+VALUES (1, 'harry styles', '01-02-1994'::date, NULL),
+       (2, 'matue', '11-10-1993'::date, NULL),
+       (3, 'imagine dragons', '14-07-1987'::date, NULL),
+	   (4, 'the weeknd', '16-02-1990'::date, NULL),
+	   (5, 'shakira', '02-02-1977'::date, NULL);
+
+INSERT INTO album (id_album, album_name, almbum_creation_date, cover, fk_producer_id_producer)
+VALUES (1, 'maquina do tempo', '10-09-2020'::date, NULL, 2),
+       (2, 'evolve', '23-06-2017'::date, NULL, 3),
+       (3, 'after hours', '20-03-2020'::date, NULL, 1),
+	   (4, 'world cup', '21-03-2014'::date, NULL, 1),
+	   (5, 'fine line', '13-12-2019'::date, NULL, 4);	   
+
+INSERT INTO "group" (id_grupo, group_name, group_creation_date)
+VALUES (1, 'licio', '30-01-2022'::date),
+       (2, 'cardoso', '09-11-2021'::date),
+       (3, 'brazil', '27-05-2021'::date),
+	   (4, 'amantes do trap', '10-09-2022'::date),
+	   (5, 'unb', '01-02-2023'::date);
+
+INSERT INTO genre (id_genre, genre_name, "description")
+VALUES (1, 'pop', NULL),
+       (2, 'trap', NULL),
+       (3, 'eletronica', NULL),
+	   (4, 'indie', NULL),
+	   (5, 'r&b', NULL);
+
+INSERT INTO radio (id_radio, radio_name)
+VALUES (1, 'mix'),
+       (2, 'joven pan'),
+       (3, 'clube fm'),
+	   (4, 'verde oliva'),
+	   (5, 'nova brasil');
+	   
+INSERT INTO user (id_user, username, date_of_birth, email, "password", phone, photo, is_admin)
+VALUES (1, 'pedro', '30-01-2022'::date, 'pedro@gmail.com', '0000', '985461205', NULL, FALSE),
+       (2, 'lucas', '09-11-2021'::date, 'lucas@gmail.com', '1111', '996563321', NULL, FALSE),
+       (3, 'arthur', '27-05-2021'::date, 'arthur@gmail.com', '2222', '987642352', NULL, FALSE),
+	   (4, 'eduardo', '10-09-2022'::date, 'eduardo@gmail.com', '1234', '996315456', NULL, TRUE),
+	   (5, 'maristela', '01-02-2023'::date, 'maristela@gmail.com', '4321', '986654631', NULL, TRUE);
+	   
+INSERT INTO "show" (id_show, video, show_name, show_creation_date)
+VALUES (1, NULL, 'lollapalooza', '20-06-1991'::date),
+	   (2, NULL, 'rock in  rio', '07-08-1985'::date),
+	   (3, NULL, 'na praia', '27-01-2013'::date),
+	   (4, NULL, 'tomorrowland', '21-07-2015'::date),
+	   (5, NULL, 'primavera summit', '29-10-2001'::date);
+	   
+INSERT INTO playlist (id_playlist, playlist_name, cover, playlist_creation_date, fk_user_id_user)
+VALUES (1, 'academia', NULL, '05-01-2023'::date, 2),
+	   (2, 'top 50 br', NULL, '13-01-2023'::date, 2),
+	   (3, 'favoritas', NULL, '17-01-2023'::date, 3),
+	   (4, 'estudos', NULL, '21-01-2023'::date, 1),
+	   (5, 'dormir', NULL, '26-01-2023'::date, 3);
+	   
+INSERT INTO music (id_musica, music_name, clip, lyrics, "language", fk_artist_id_artist, fk_genre_id_genre, fk_album_id_album)
+VALUES (1, 'watermelon sugar', NULL, NULL, 'ingles', 1, 1, 3),
+	   (2, 'as it was', NULL, NULL, 'ingles', 1, 4, 3),
+	   (3, 'waka waka', NULL, NULL, 'espanhol', 5, 5, 4),
+	   (4, 'lalala', NULL, NULL, 'espanhol', 5, 1, 4),
+	   (5, 'blinding lights', NULL, NULL, 'ingles',4, 3, 5),
+	   (6, 'die for you', NULL, NULL, 'ingles', 4, 2, 5),
+	   (7, 'kenny g', NULL, NULL, 'portugues', 2, 2, 1),
+	   (8, 'quer voar', NULL, NULL, 'portugues', 2, 2, 1),
+	   (9, 'radioactive', NULL, NULL, 'ingles', 3, 3, 2),
+	   (10, 'believer', NULL, NULL, 'ingles', 3, 5, 2);
